4b706d8d5a72d6c59a440ea55b74cc40f07b1b62
Fabio Baruffa <fbaru.dev@gmail.com>
2016-10-01 14:19:16 +0200

    Create simd-example.c

---
+++ examples/simd-example.c
@@ -0,0 +1,8 @@
+//Compile: icc -qopt-report=2 -c simd-example.c
+
+void add_floats(float *a, float *b, float *c, float *d, float *e, int n) {
+ int i;
+ for (i=0; i<n; i++){
+  a[i] = a[i] + b[i] + c[i] + d[i] + e[i];
+ }
+}
